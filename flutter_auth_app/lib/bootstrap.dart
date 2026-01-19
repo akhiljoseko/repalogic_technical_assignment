f736b1e08dde482942edad8a35cf27e1331ebeb1
Akhil Jose <dev.akhiljoseko@gmail.com>
2026-01-19 20:18:01 +0530

    Refactor(login): Use responsive builder and layouts

--- flutter_auth_app/lib/bootstrap.dart
+++ flutter_auth_app/lib/bootstrap.dart
@@ -1,33 +1,22 @@
 import 'dart:async';
 import 'dart:developer';
 
-import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
-
-class AppBlocObserver extends BlocObserver {
-  const AppBlocObserver();
-
-  @override
-  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
-    super.onChange(bloc, change);
-    log('onChange(${bloc.runtimeType}, $change)');
-  }
-
-  @override
-  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
-    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
-    super.onError(bloc, error, stackTrace);
-  }
-}
+import 'package:flutter_auth_app/modules/authentication/data/repositories/authentication_repository_impl.dart';
+import 'package:flutter_auth_app/modules/authentication/domain/repositories/authentication_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
-  Bloc.observer = const AppBlocObserver();
-
   // Add cross-flavor configuration here
 
-  runApp(await builder());
+  runApp(
+    RepositoryProvider<AuthenticationRepository>(
+      create: (_) => AuthenticationRepositoryImpl(),
+      child: await builder(),
+    ),
+  );
 }
---
+++ flutter_auth_app/lib/modules/authentication/presentation/login/layouts/login_compact_layout.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth_app/core/utils/context_extensions.dart';
+import 'package:flutter_auth_app/core/utils/snackbar_utils.dart';
+import 'package:flutter_auth_app/gen/assets.gen.dart';
+import 'package:flutter_auth_app/l10n/l10n.dart';
+import 'package:flutter_auth_app/modules/authentication/presentation/login/cubit/login_cubit.dart';
+import 'package:flutter_auth_app/modules/authentication/presentation/widgets/question_text_button.dart';
+import 'package:flutter_auth_app/shared/widgets/email_text_field.dart';
+import 'package:flutter_auth_app/shared/widgets/password_text_field.dart';
+import 'package:flutter_auth_app/shared/widgets/primary_button.dart';
+import 'package:flutter_auth_app/shared/widgets/screen_padding.dart';
+import 'package:flutter_auth_app/shared/widgets/spacing.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+
+class LoginCompactLayout extends StatefulWidget {
+  const LoginCompactLayout({super.key});
+
+  @override
+  State<LoginCompactLayout> createState() => _LoginCompactLayoutState();
+}
+
+class _LoginCompactLayoutState extends State<LoginCompactLayout> {
+  final _formKey = GlobalKey<FormState>();
+
+  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
+  late final _emailController = TextEditingController();
+  late final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<LoginCubit, LoginState>(
+      listener: (context, state) {
+        if (state is LoginError) {
+          SnackbarUtils.showErrorSnackBar(
+            context,
+            content: state.exception.localize(context.l10n),
+          );
+          return;
+        }
+      },
+      child: Scaffold(
+        body: ScreenHorizontalPadding(
+          child: Center(
+            child: Form(
+              key: _formKey,
+              autovalidateMode: _autovalidateMode,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    Assets.svg.repalogicLogo.path,
+                  ),
+                  const Vspace(50),
+                  Text(
+                    'Welcome to Repalogic',
+                    style: context.textTheme.titleLarge,
+                  ),
+                  const Vspace(12),
+                  Text(
+                    'Login to continue',
+                    style: context.textTheme.titleMedium,
+                  ),
+                  const Vspace(16),
+                  EmailTextField(controller: _emailController),
+                  const Vspace(4),
+                  PasswordTextField(controller: _passwordController),
+                  const Vspace(16),
+                  SizedBox(
+                    width: double.maxFinite,
+                    child: PrimaryButton(
+                      onPressed: () {
+                        if (!_formKey.currentState!.validate()) {
+                          setState(() {
+                            _autovalidateMode =
+                                AutovalidateMode.onUserInteraction;
+                          });
+                          return;
+                        }
+                        context.read<LoginCubit>().loginWithPassword(
+                          email: _emailController.text.trim(),
+                          password: _passwordController.text.trim(),
+                        );
+                      },
+                      buttonLabel: 'Sign in',
+                    ),
+                  ),
+                  const Vspace(16),
+                  QuestionTextButton(
+                    onPressed: () {},
+                    question: "Don't have an account? ",
+                    buttonLabel: 'Sign up',
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
