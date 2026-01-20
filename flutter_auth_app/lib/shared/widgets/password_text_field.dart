7c102e69177188d6205a0e6646bcbe89ec3a4aec
Akhil Jose <dev.akhiljoseko@gmail.com>
2026-01-20 10:38:13 +0530

    fix: Obscured password text field

--- flutter_auth_app/lib/shared/widgets/password_text_field.dart
+++ flutter_auth_app/lib/shared/widgets/password_text_field.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_auth_app/core/utils/form_validators.dart';
 import 'package:flutter_auth_app/l10n/l10n.dart';
 
-class PasswordTextField extends StatelessWidget {
+class PasswordTextField extends StatefulWidget {
   const PasswordTextField({
     this.controller,
     super.key,
@@ -11,16 +11,37 @@ class PasswordTextField extends StatelessWidget {
   final TextEditingController? controller;
 
   @override
+  State<PasswordTextField> createState() => _PasswordTextFieldState();
+}
+
+class _PasswordTextFieldState extends State<PasswordTextField> {
+  bool _isObscured = true;
+
+  @override
   Widget build(BuildContext context) {
     return TextFormField(
-      controller: controller,
-      decoration: const InputDecoration(hintText: 'Password', helperText: ''),
+      controller: widget.controller,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        helperText: '',
+        suffixIcon: IconButton(
+          icon: Icon(
+            _isObscured ? Icons.visibility : Icons.visibility_off,
+          ),
+          onPressed: () {
+            setState(() {
+              _isObscured = !_isObscured;
+            });
+          },
+        ),
+      ),
       validator: (value) => FormValidators.validateMinLength(
         value: value,
         minLength: 8,
         fieldName: 'Password',
         l10n: context.l10n,
       ),
+      obscureText: _isObscured,
     );
   }
 }
