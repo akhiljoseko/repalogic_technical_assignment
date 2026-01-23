084929765798cafb2a4a20a4ad6a17f316273491
Akhil Jose <dev.akhiljoseko@gmail.com>
2026-01-23 12:52:56 +0530

    Chore: Remove place holder files

--- flight_booking_app/lib/app/view/app.dart
+++ flight_booking_app/lib/app/view/app.dart
@@ -1,5 +1,5 @@
-import 'package:flight_booking_app/counter/counter.dart';
 import 'package:flight_booking_app/l10n/l10n.dart';
+import 'package:flight_booking_app/modules/flights/presentation/flight_search/flight_search_screen.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -16,7 +16,7 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: const FlightSearchScreen(),
     );
   }
 }
